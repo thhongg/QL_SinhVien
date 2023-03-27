ce774887f7b1662d47e29c5b350b9d5603ce9a6c
tai <cvtai105@gmail.com>
2023-03-28 00:31:03 +0700

    add .h .cpp file

---
+++ Project1/QLSinhVien.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "SinhVien.h"
+class QLSinhVien {
+private:
+    SinhVien* dsSinhVien; // Mảng động chứa danh sách sinh viên
+    int soSinhVien; // Số lượng sinh viên trong danh sách
+    bool isLoaded; // Biến đánh dấu xem danh sách đã được tải từ file chưa
+
+public:
+    QLSinhVien(); // Constructor mặc định
+    ~QLSinhVien(); // Destructor
+    void themSinhVien(); // Thêm một sinh viên mới vào danh sách
+    void capNhatFile(); // Cập nhật danh sách sinh viên vào file
+    void docDanhSach(); // Đọc danh sách sinh viên từ file
+    void xuatDanhSach(); // Xuất danh sách sinh viên
+    void timSinhVienYeu(); // Tìm và xuất danh sách sinh viên có điểm trung bình dưới trung bình lớp
+    void xepLoaiSinhVien(); // Xếp loại sinh viên và xuất danh sách sinh viên có thông tin xếp loại
+    void timSinhVienSinhTrongNgay(); // Tìm và xuất danh sách sinh viên có sinh nhật trong ngày hiện tại
+};
+
---
+++ Project1/SinhVien.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+class SinhVien {
+private:
+    char* hoTen; // Họ và tên
+    char* maSV; // Mã số sinh viên
+    char* ngaySinh; // Ngày sinh
+    float marks[3]; // Mảng điểm: bài tập, thi giữa kỳ, thi cuối kỳ
+    float stdAvg; // Điểm trung bình
+
+public:
+    SinhVien(); // Constructor mặc định
+    SinhVien(char* hoTen, char* maSV, char* ngaySinh, float* marks); // Constructor có tham số
+    SinhVien(const SinhVien& sv); // Constructor sao chép
+    ~SinhVien(); // Destructor
+    SinhVien& operator=(const SinhVien& sv); // Toán tử gán bằng
+    void nhapThongTin(); // Nhập thông tin sinh viên
+    void xuatThongTin(); // Xuất thông tin sinh viên
+    void tinhDiemTrungBinh(); // Tính điểm trung bình
+};
